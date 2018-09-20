e8d4e72aa5e68b77399109a2f90efa3f9f9e8de0
razzester <razzester86@gmail.com>
2018-09-20 12:56:19 +0545

    Dummy

---
+++ api/dummy.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify
+from flask.views import MethodView
+from models.event import Event
+
+
+
+def add_event():
+        print('######################')
+        print (request.form.name)
+        print('######################')
+        # event=Event()
+        # event.name=request.data['name']
+        # event.description=request.data['description']
+        # event.active_shows_id=request.data['active_shows_id']
+        # event.put()
+        return "Success"
+
+def add_event_dummy():
+        event=Event()
+        event.name='sadfasdf'
+        event.description='adsfasdfasd'
+        event.active_shows_id=''
+        event.put()
+        return "Success"
+
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from events import HelloView
+from api.dummy import add_event_dummy, add_event
 
 
 def app_add_urls(app):
     app.add_url_rule('/hello', view_func=HelloView.as_view('hello_view'), methods=['get', 'post'])
+    app.add_url_rule('/dummy', 'adding event', add_event, methods=['post'])
     return app
