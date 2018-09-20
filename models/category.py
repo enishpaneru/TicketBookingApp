dc8e1582f0c55ab0c3b3516374ee6331f6337318
razzester <razzester86@gmail.com>
2018-09-20 15:19:58 +0545

    Dummy Post requests

--- models/category.py
+++ models/category.py
@@ -4,5 +4,5 @@ from google.appengine.ext import ndb
 class Category(ndb.Model):
     """Model categories of seat."""
     category_name=ndb.StringProperty()
-    screen_id=ndb.keyProoperty()
+    screen_id=ndb.KeyProperty()
     seats=ndb.JsonProperty()
--- models/screen_layout.py
+++ models/screen_layout.py
@@ -4,7 +4,7 @@ from google.appengine.ext import ndb
 class Screen_Layout(ndb.Model):
     """Model and individual screen_layout for clients."""
     screen_name=ndb.StringProperty()
-    client_id=ndb.keyProoperty()
+    client_id=ndb.KeyProperty()
     location=ndb.StringProperty()
     max_rows=ndb.IntegerProperty()
     max_columns=ndb.IntegerProperty()
